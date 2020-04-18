511becd07be7e5aa92ed08ae13aa4b44d627dbb9
xu0105 <215790428@qq.com>
2020-04-18 22:58:32 +0800

    fda

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import "./home.less";
 
 export default class Home extends Component {
   render() {
+    fdfadfda == 1;
     dasd == 1;
     return <div className="home">this is home ~hi xht</div>;
   }
